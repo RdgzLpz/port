991ed0eca6b6f4ca59d35b02ecd5b782863d8aae
Oscar Rodriguez <varios_varios@outlook.com>
2020-10-18 21:29:11 -0400

    added a slider

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
+const icon = document.querySelector( '#go-up-button' );
+const goUpIcon = document.querySelector( '#go-up-icon' );
+
 let footerYear = document.querySelector('#footer-year');
 footerYear.innerHTML = new Date( ).getFullYear( );
+
+// Setting go up button 
+window.addEventListener( 'scroll', ( ) => {
+    if ( pageYOffset > 600 ) 
+        icon.style.display = 'block';
+    else 
+        icon.style.display = 'none'; 
+} )
+
+setInterval( ( ) => {
+    goUpIcon.className = 'animation-icon'
+}, 2000 )
+
+setInterval( ( ) => {
+    goUpIcon.classList.remove( 'animation-icon' );     
+}, 3500 )
